72631dfea5b0862d857fc52997c941c47a56bd9c
jdg2896 <jasper.d.gabriel@gmail.com>
2026-04-08 22:15:27 +0800

    feat: respond to multiple ping commands (first attempt)

--- app/main.py
+++ app/main.py
@@ -7,15 +7,16 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     while True:
-        client_socket, client_address = server_socket.accept() # wait for client
-        data = client_socket.recv(1024)  # receive data from client
+        print("Waiting for client connections...")
+        connection, client_address = server_socket.accept() # wait for client
+        data = connection.recv(1024)  # receive data from client
 
         print(f"Client connected from {client_address}")
         print(f"Received data: {data}")
         if data.startswith(b"*1\r\n$4\r\nPING\r\n"):  # check if command is PING
             print(f"Received PING command: Responding with PONG to client...")
-            client_socket.sendall(b"+PONG\r\n")  # send response to client
-            client_socket.close()  # close client connection
+            connection.sendall(b"+PONG\r\n")  # send response to client
+            connection.close()  # close client connection
 
 
 if __name__ == "__main__":
