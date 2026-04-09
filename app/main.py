4e57251c7b00ce6afbebcc8e66ef2bb835a11cec
jdg2896 <jasper.d.gabriel@gmail.com>
2026-04-09 12:12:20 +0800

    feat: implement echo command

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import asyncio
 
 PING = b"*1\r\n$4\r\nPING\r\n"
 PONG = b"+PONG\r\n"
+ECHO = b"*2\r\n$4\r\nECHO\r\n"
 
 async def main():
     print("Starting Redis server on localhost:6379...")
@@ -24,6 +25,13 @@ async def _handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWri
             print("Sending PONG to client:", writer.get_extra_info("peername"))
             writer.write(PONG)
             await writer.drain()
+        elif data.startswith(ECHO):
+            print("Received ECHO from client:", writer.get_extra_info("peername"))
+            message = data[len(ECHO):].strip()
+            print("Echoing back to client:", writer.get_extra_info("peername"), "Message:", message)
+            writer.write(b"+" + message + b"\r\n")
+            await writer.drain()
+
     writer.close()
     await writer.wait_closed()
     print("Client disconnected:", writer.get_extra_info("peername"))
