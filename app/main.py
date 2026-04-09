b22744a4346e55d2ec6db2e582183542e1f623a4
jdg2896 <jasper.d.gabriel@gmail.com>
2026-04-09 11:53:04 +0800

    feat: handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import socket
+# import socket
 import asyncio
 
 PING = b"*1\r\n$4\r\nPING\r\n"
@@ -6,40 +6,20 @@ PONG = b"+PONG\r\n"
 
 async def main():
     print("Starting Redis server on localhost:6379...")
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server = await asyncio.start_server(_handle_client, "localhost", 6379)
 
     print("Waiting for client connections...")
-    client_connection, client_address = server_socket.accept() # wait for client
-
-    await _handle_client(client_connection, client_address)
-
-
-async def _handle_client(client_connection: socket.socket, client_address):
-    print(f"Client connected from {client_address}")
-    with client_connection:
-            data = client_connection.recv(1024)  # receive data from client
-
-            print(f"Client connected from {client_address}")
-            print(f"Received data: {data}")
-            if data.startswith(PING):
-                _send_pong(client_connection)
-
-                while True:
-                    data = client_connection.recv(1024)  # wait for more data from client
-                    if not data:
-                        print(f"Client {client_address} disconnected.")
-                        break  # client disconnected
-                    print(f"Received data: {data}")
-                    if data.startswith(PING):
-                        _send_pong(client_connection)
-                    else:
-                        print(f"Received unknown command: {data}")
-                        client_connection.sendall(b"-ERR unknown command\r\n")  # send error response to client
+    async with server:
+        await server.serve_forever()
 
 
-def _send_pong(client_connection: socket.socket):
-    print(f"Received PING command: Responding with PONG to client...")
-    client_connection.sendall(PONG)
+async def _handle_client(reader, writer):
+    data = await reader.read(1024)   # non-blocking
+    print(f"Received data: {data!r}")
+    if data == PING:
+        writer.write(PONG)
+        await writer.drain()
+    writer.close()
 
 
 if __name__ == "__main__":
