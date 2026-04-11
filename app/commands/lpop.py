2aea32ee5a93e2d4356a0f4fc3d9fc51ccba5ed9
Jasper Gabriel <jasper.d.gabriel@gmail.com>
2026-04-11 15:24:59 +0800

    feat: handle lpop response depending on condition

--- app/commands/lpop.py
+++ app/commands/lpop.py
@@ -1,13 +1,25 @@
 from app.constants import NIL
 from app.types import DataStore
-from app.utils import to_resp_bulk_string
+from app.utils import to_resp_array, to_resp_bulk_string
 
 
 def handle(args: list[bytes], data_store: DataStore) -> bytes:
     key = args[0]
+    number = int(args[1]) if len(args) > 1 else 1
     if key not in data_store:
         return NIL
     # Remove and return the first element from the list
-    value = data_store[key][0].pop(0)
-    print("LPOP result for key:", key, "Value:", value)
-    return to_resp_bulk_string(value)
+    list = data_store[key][0]
+    values = list[:number]
+    if not list:
+        return NIL
+    for _ in range(number):
+        values = list.pop(0)
+    
+    print("LPOP result for key:", key, "Value:", values)
+    if len(values) == 1:
+        return to_resp_bulk_string(values[0])
+    elif len(values) > 1:
+        return to_resp_array(values)
+    else:
+        return NIL
