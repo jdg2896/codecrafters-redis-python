ceb8607eca6357ff81e51e0d761b5ff2b9433d79
Jasper Gabriel <jasper.d.gabriel@gmail.com>
2026-04-11 15:13:38 +0800

    fix: reverse list for lpush command

--- app/commands/lpush.py
+++ app/commands/lpush.py
@@ -8,5 +8,6 @@ def handle(args: list[bytes], data_store: DataStore) -> bytes:
     if key not in data_store:
         data_store[key] = ([], None)
     # Prepend values to the list
-    data_store[key][0][:0] = values
+    data_store[key][0][:0] = list(reversed(values))
+    print("LPUSH result for key:", key, "Values:", data_store[key][0])
     return to_resp_integer(len(data_store[key][0]))
