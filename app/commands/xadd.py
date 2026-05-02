e7199d171bed1674e6a554fd236209800e0128c0
jdg2896 <jasper.d.gabriel@gmail.com>
2026-05-02 14:47:02 +0800

    feat: autogenerate entry id sequence number for xadd [skip ci]
    feat: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id sequence number for xadd [skip ci]
    
    fix: autogenerate entry id when milliseconds increment [skip ci]

--- app/commands/xadd.py
+++ app/commands/xadd.py
@@ -9,9 +9,21 @@ def handle(args: list[bytes], data_store: DataStore) -> bytes:
 	stream_key = args[0]
 	entry_id = args[1]
 	key_value_pairs = args[2:]
+	stream = data_store.get(stream_key, ([], None))[0]
 
 	if entry_id == b"0-0":
 		return to_resp_error(ENTRY_ID_NOT_ZERO_ERROR)
+	
+	# The entry ID can be specified as either a specific ID in the format of <millisecondsTime>-<sequenceNumber> or as an auto-generated ID using the special ID of '*'.
+	new_milliseconds_time, new_sequence_number = (
+		int(x) if x != b'*' else '*' for x in entry_id.split(b'-')
+	)
+	if new_sequence_number == '*':
+		new_sequence_number = autogenerate_sequence_number(
+			stream,
+			int(new_milliseconds_time)
+		)
+		entry_id = f"{new_milliseconds_time}-{new_sequence_number}".encode()
 
 	# Validate that the entry id:
 	# - is in the format of <millisecondsTime>-<sequenceNumber>
@@ -19,10 +31,11 @@ def handle(args: list[bytes], data_store: DataStore) -> bytes:
 	# - sequenceNumber is a valid integer
 	# - millisecondsTime must be greater than or equal to the last entry's millisecondsTime
 	# - if the millisecondsTime values are equal, the sequenceNumber of the new ID must be greater than the last entry's sequenceNumber
-	for entry in data_store.get(stream_key, ([], None))[0]:
+	for entry in stream:
 		last_entry_id = entry[0]
 		last_milliseconds_time, last_sequence_number = map(int, last_entry_id.split(b'-'))
 		new_milliseconds_time, new_sequence_number = map(int, entry_id.split(b'-'))
+
 
 		if new_milliseconds_time < last_milliseconds_time:
 			return to_resp_error(ENTRY_ID_EQUAL_OR_SMALLER_ERROR)
@@ -38,3 +51,19 @@ def handle(args: list[bytes], data_store: DataStore) -> bytes:
 	data_store[stream_key] = (stream, None)
 
 	return to_resp_bulk_string(entry_id)
+
+
+def autogenerate_sequence_number(stream: list[tuple[bytes, list[bytes]]], milliseconds_time: int) -> int:
+	'''
+	Autogenerates a sequence number for a new stream entry based on the existing entries in the stream.
+	- If the stream is empty for a given time part, the sequence number starts at 0.
+	- If there are already entries with the same time part, the new sequence number is the last sequence number + 1.
+	- The only exception is when the time part is 0. In that case, the default sequence number starts at 1.
+	'''
+	if not stream:
+		return 1 if milliseconds_time == 0 else 0
+	last_entry_id = stream[-1][0]
+	last_milliseconds_time, last_sequence_number = map(int, last_entry_id.split(b'-'))
+	if milliseconds_time == last_milliseconds_time:
+		return last_sequence_number + 1
+	return 0
