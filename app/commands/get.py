e1c87605103863979c2854a91f3b7d8ba3234e37
jdg2896 <jasper.d.gabriel@gmail.com>
2026-05-03 11:57:12 +0800

    fix: coerce different types to string bytes for get command [skip ci]

--- app/commands/get.py
+++ app/commands/get.py
@@ -9,6 +9,8 @@ def handle(args: list[bytes], data_store: DataStore) -> bytes:
     key = args[0]
     value = data_store.get(key)
     if value is not None and (value[1] is None or value[1] > time.time()):
-        return to_resp_bulk_string(value[0])
+        # If value is not bytes, convert it to string and then to bytes
+        data = value[0] if isinstance(value[0], bytes) else str(value[0]).encode()
+        return to_resp_bulk_string(data)
     else:
         return NIL
