50bf2a41935aeba98241d450d24a9e9b9649a810
jdg2896 <jasper.d.gabriel@gmail.com>
2026-05-03 12:08:38 +0800

    feat: return error when incr existing key with non-int value [skip ci]

--- app/commands/incr.py
+++ app/commands/incr.py
@@ -1,11 +1,22 @@
 from app.types import DataStore
-from app.utils import to_resp_integer
+from app.utils import to_resp_integer, to_resp_error
+
+NOT_AN_INTEGER_ERROR = b"ERR value is not an integer or out of range"
 
 
 async def handle(args: list[bytes], data_store: DataStore) -> bytes:
     key = args[0]
     # Get the current value, defaulting to 0 if the key doesn't exist or has expired
-    value = int(data_store.get(key, (0, None))[0])
+    value = data_store.get(key, (0, None))[0]
+
+    if isinstance(value, bytes):
+        try:
+            value = int(value)
+        except ValueError:
+            return to_resp_error(NOT_AN_INTEGER_ERROR)
+    elif not isinstance(value, int):
+        return to_resp_error(NOT_AN_INTEGER_ERROR)
+
     value += 1
     data_store[key] = (value, None)
     return to_resp_integer(value)
